233dfc5750df11eb75d6598d1a964528d895a4eb
Dabo Ross <daboross@daboross.net>
2014-07-12 07:36:16 -0700

    Fix some imports + package

--- src/main/java/net/daboross/bukkitdev/undergroundmahem/UndergroundMahemPlugin.java
+++ src/main/java/net/daboross/bukkitdev/undergroundmahem/UndergroundMahemPlugin.java
@@ -14,10 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.daboross.bukkitdev.underground-mahem;
+package net.daboross.bukkitdev.undergroundmahem;
 
 import java.io.IOException;
-import java.util.logging.Level;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
