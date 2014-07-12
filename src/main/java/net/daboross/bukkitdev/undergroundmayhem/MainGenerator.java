b525b629e7af137e507590c96669fcbb62af65fc
Dabo Ross <daboross@daboross.net>
2014-07-12 09:22:02 -0700

    Redo some generation stuff!

--- src/main/java/net/daboross/bukkitdev/undergroundmayhem/MainGenerator.java
+++ src/main/java/net/daboross/bukkitdev/undergroundmayhem/MainGenerator.java
@@ -21,41 +21,37 @@ import java.util.List;
 import java.util.Random;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Biome;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.util.noise.NoiseGenerator;
-import org.bukkit.util.noise.SimplexNoiseGenerator;
+import org.bukkit.util.noise.OctaveGenerator;
+import org.bukkit.util.noise.SimplexOctaveGenerator;
 
 public class MainGenerator extends ChunkGenerator {
 
     private static final int WALLS_DISTANCE = 100;
-    private SimplexNoiseGenerator noiseGenerator;
+    private SimplexOctaveGenerator noiseGenerator;
 
-    private SimplexNoiseGenerator getNoiseGenerator(World world) {
+    private SimplexOctaveGenerator getNoiseGenerator(World world) {
         if (noiseGenerator == null) {
-            noiseGenerator = new SimplexNoiseGenerator(world);
+            noiseGenerator = new SimplexOctaveGenerator(world, 5);
         }
+        noiseGenerator.setScale(1 / 256.0);
         return noiseGenerator;
     }
 
     @SuppressWarnings("deprecation")
     @Override
-    public byte[][] generateBlockSections(final World world, final Random random, final int chunkX, final int chunkZ, final BiomeGrid biomes) {
-        NoiseGenerator generator = getNoiseGenerator(world);
-        for (int x = 0; x < 16; x++) {
-            for (int z = 0; z < 16; z++) {
-                biomes.setBiome(x, z, Biome.FLOWER_FOREST);
-            }
-        }
+    public byte[][] generateBlockSections(final World world, final Random random, final int chunkX, final int chunkZ, BiomeGrid grid) {
+        OctaveGenerator generator = getNoiseGenerator(world);
         byte[][] result = new byte[world.getMaxHeight() / 16][];
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                int bedrockHeight = (int) (1 + generator.noise(chunkX * 16 + x, chunkZ * 16 + z) * 5);
-                int stoneHeight = (int) (60 + generator.noise(chunkX * 16 + x, chunkZ * 16 + z) * 20);
-                int obsidianHeight = stoneHeight + (int) generator.noise(chunkX * 16 + x, chunkZ * 16 + z);
-                int bedrockTop = (int) (world.getMaxHeight() - 1 - -generator.noise(chunkX * 16 + x, chunkZ * 16 + z) * 5);
-                int glowStoneTop = (int) (world.getMaxHeight() - 40 - generator.noise(chunkX * 16 + x, chunkZ * 16 + z) * 5);
+                int bedrockHeight = (int) (1 + generator.noise(chunkX * 16 + x, chunkZ * 16 + z, 0.5, 0.5));
+                int stoneHeight = (int) (60 + generator.noise(chunkX * 16 + x, chunkZ * 16 + z, 0.5, 0.5));
+                int obsidianHeight = stoneHeight + (int) generator.noise(chunkX * 16 + x, chunkZ * 16 + z, 0.5, 0.5);
+                if (obsidianHeight > world.getMaxHeight()) {
+                    obsidianHeight = world.getMaxHeight();
+                }
                 for (int y = 0; y < obsidianHeight; y++) {
                     if (result[y >> 4] == null) {
                         result[y >> 4] = new byte[4096];
@@ -70,21 +66,8 @@ public class MainGenerator extends ChunkGenerator {
                     }
                     result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = (byte) material.getId();
                 }
-                for (int y = world.getMaxHeight() - 1; y > glowStoneTop; y--) {
-                    if (result[y >> 4] == null) {
-                        result[y >> 4] = new byte[4096];
-                    }
-                    Material material;
-                    if (y >= bedrockTop) {
-                        material = Material.BEDROCK;
-                    } else {
-                        material = Material.GLOWSTONE;
-                    }
-                    result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = (byte) material.getId();
-                }
             }
         }
-
         return result;
     }
 
